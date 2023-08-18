e8cfeac29f6571b2825db727a52c9c6543eecf3a
Yi JIN <117509646+MarcelProustOVO@users.noreply.github.com>
2023-08-18 18:48:32 +0800

    Initial commit

---
+++ Assets/Core/AI/Tasks/InitBoss.cs
@@ -0,0 +1,16 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+using Core.Bosses;
+using Core.UI;
+using UnityEngine;
+
+namespace Core.AI
+{
+    public class InitBoss : EnemyAction
+    {
+        public override TaskStatus OnUpdate()
+        {
+            GuiManager.Instance.ShowBossName(GetComponent<BossConfig>().bossName);
+            return TaskStatus.Success;
+        }
+    }
+}
---
+++ Assets/Core/AI/Tasks/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
+using Core.Character;
+using Core.Combat;
+using UnityEngine;
+
+namespace Core.AI
+{
+    public class Shoot : EnemyAction
+    {
+        public List<Weapon> weapons;
+        public bool shakeCamera;
+
+        public override TaskStatus OnUpdate()
+        {
+            foreach (var weapon in weapons)
+            {
+                var projectile = Object.Instantiate(weapon.projectilePrefab, weapon.weaponTransform.position,
+                    Quaternion.identity);
+                projectile.Shooter = gameObject;
+                
+                var force = new Vector2(weapon.horizontalForce * transform.localScale.x, weapon.verticalForce);
+                projectile.SetForce(force);
+                
+                if (shakeCamera)
+                    CameraController.Instance.ShakeCamera(0.5f);
+            }
+            
+            return TaskStatus.Success;
+        }
+    }
+}
